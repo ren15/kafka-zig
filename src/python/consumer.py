0f5cb1a5780da8442095a0ea3e14e2621df2a94f
Ren Wang <iamwrm@gmail.com>
2023-10-11 19:58:37 +0800

    use set_offset_last_N

--- src/python/consumer.py
+++ src/python/consumer.py
@@ -1,11 +1,35 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
 
 cosumer = KafkaConsumer("test", bootstrap_servers="localhost:19092")
 
-last_position = cosumer.position(cosumer.assignment()[0])
+
+def set_offset_last_N(consumer, topics, last_n):
+    """
+    Set all topics to last_n messages from the end.
+
+    """
+    tp_list = [TopicPartition(topic, 0) for topic in topics]
+    consumer.assign(tp_list)
+    consumer.seek_to_end()
+
+    last_positions = {}
+    for tp in tp_list:
+        last_position = consumer.position(tp)
+        last_positions[tp.topic] = last_position
+
+        if last_position < last_n:
+            position_to_set = 0
+        else:
+            position_to_set = last_position - last_n
+
+        consumer.seek(tp, position_to_set)
+    return last_positions
+
+
+last_position = set_offset_last_N(cosumer, ["test"], 1000)
 
 
 for message in cosumer:
     print(message)
-    if message.offset == last_position - 1:
+    if message.offset == last_position[message.topic]:
         break
