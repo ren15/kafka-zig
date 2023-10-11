654afac29098b951e4c0437a4db0628288c805fc
Ren Wang <iamwrm@gmail.com>
2023-10-11 13:36:52 +0000

    wait for redpanda to start

--- scripts/redpanda/start.sh
+++ scripts/redpanda/start.sh
@@ -9,4 +9,12 @@ docker ps -a
 
 docker logs redpanda-0
 
+# loop for 10 seconds to check cpu usage
+for i in {1..10}
+do
+    echo "---------------------"
+    docker logs redpanda-0
+    docker stats redpanda-0 --no-stream
+    sleep 1
+done
 
