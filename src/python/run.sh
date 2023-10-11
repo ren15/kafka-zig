cfc41b7ce5b8e9b4e3bde6f56a75fb9555e73fa5
Ren Wang <iamwrm@gmail.com>
2023-10-11 13:44:13 +0000

    works

--- src/python/run.sh
+++ src/python/run.sh
@@ -12,8 +12,10 @@ python src/python/producer.py
 
 free -h
 
+echo "Starting consumer, ci seems to have a problem with this"
 python src/python/consumer.py &
 
 free -h
 
+echo "Sleeping for 20 seconds"
 sleep 20
