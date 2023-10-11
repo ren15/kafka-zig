39c8f9c0875089360aad7bbe98a0f66a2beb9221
Ren Wang <iamwrm@gmail.com>
2023-10-11 13:58:03 +0000

    checking

--- src/python/run.sh
+++ src/python/run.sh
@@ -6,25 +6,25 @@ export PYTHONBUFFERED=1
 
 pip install -r src/python/requirements.txt
 
-free -h
-
 echo "Sleeping for 60 seconds"
 sleep 60
 
+echo "Starting producer"
 python src/python/producer.py
+echo "Producer finished"
 
-free -h
 
 echo "Starting consumer, ci seems to have a problem with this"
 
 docker rm -f consumer || true
 docker run \
+    --name=consumer \
     -d \
     --network=host \
-    --name=consumer \
     -v "$(pwd)/src/python":/app \
     python:3.11.6 \
-    bash -c "pip install -r /app/requirements.txt && python /app/consumer.py > /app/consumer.log 2>&1"
+    bash -c "pip install -r /app/requirements.txt && \
+        python /app/consumer.py > /app/consumer.log 2>&1"
 
 docker logs consumer
 
@@ -34,4 +34,6 @@ sleep 60
 
 
 docker logs consumer
+
+ls -lah src/python/consumer.log
 cat src/python/consumer.log
