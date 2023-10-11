4a0eac725375d160b3afd5d92cd665f8d15082e9
Ren Wang <iamwrm@gmail.com>
2023-10-11 14:42:56 +0000

    fixing

--- src/c/setup.sh
+++ src/c/setup.sh
@@ -6,9 +6,16 @@ pushd `dirname $0`
 
 wget -qc https://github.com/confluentinc/librdkafka/archive/refs/tags/v2.2.0.zip
 
-unzip -f v2.2.0.zip
+# unzip and ignore if already exists
 
-mkdir -p lib 
+ls -lah 
+
+unzip -o v2.2.0.zip || true
+
+ls -lah 
+
+mkdir -p install_prefix
+
 cd librdkafka-2.2.0
 ./configure --prefix=$PWD/../install_prefix
 make -j$(nproc)
