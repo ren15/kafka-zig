c5b2e1e60f33b3e3da1e53c89cae97779aba260b
iamwrm <iamwrm@gmail.com>
2023-10-13 22:05:44 +0800

    works

--- src/zig/setup.sh
+++ src/zig/setup.sh
@@ -4,13 +4,6 @@ set -xueo pipefail
 
 curl -fsSL https://bun.sh/install | bash
 
-ls -lah /home/runner/.bashrc
-
-echo $HOME
-
-cat /home/runner/.bashrc
-
-. /home/runner/.bashrc 
 
 export BUN_INSTALL="$HOME/.bun"
 export PATH=$BUN_INSTALL/bin:$PATH
@@ -18,3 +11,4 @@ export PATH=$BUN_INSTALL/bin:$PATH
 bun install -g @oven/zig
 
 zig version
+
